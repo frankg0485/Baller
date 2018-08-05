1da7c8d9ac279b548f9b44654551876b51cc9e23
Frank Gao <frank@swoag.com>
2018-08-05 09:49:45 -0500

    xcode storyboard files for divided balls

---
+++ Baller/ScoreView.swift
@@ -0,0 +1,21 @@
+//
+//  ScoreView.swift
+//  Baller
+//
+//  Created by Frank Gao on 7/10/18.
+//  Copyright © 2018 Frank Gao. All rights reserved.
+//
+
+import UIKit
+
+class ScoreView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
